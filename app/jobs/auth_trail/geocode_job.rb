cf0290db2eb5a904703c816bedacfe1a709f32d0
Tim Williams <timwemail@gmail.com>
2018-06-05 20:58:23 +0100

    support Telize geo results

--- app/jobs/auth_trail/geocode_job.rb
+++ app/jobs/auth_trail/geocode_job.rb
@@ -2,18 +2,18 @@ module AuthTrail
   class GeocodeJob < ActiveJob::Base
     def perform(login_activity)
       result =
-        begin
-          Geocoder.search(login_activity.ip).first.try(:data)
-        rescue => e
-          Rails.logger.info "Geocode failed: #{e.message}"
-          nil
-        end
+      begin
+        Geocoder.search(login_activity.ip).first.try(:data)
+      rescue => e
+        Rails.logger.info "Geocode failed: #{e.message}"
+        nil
+      end
 
       if result
         login_activity.update!(
           city: result["city"].presence,
-          region: result["region_name"].presence,
-          country: result["country_name"].presence
+          region: result["region_name"].presence || result["region"].presence,
+          country: result["country_name"].presence || result["country"].presence
         )
       end
     end
